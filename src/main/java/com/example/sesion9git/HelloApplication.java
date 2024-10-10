db6e98b468b8a4bad4b4726837e5117fe3bc5b89
Juan Rincón <juandavidrinconl@gmail.com>
2024-10-10 16:37:14 -0500

    Ultimos cambios

--- src/main/java/com/example/sesion9git/HelloApplication.java
+++ src/main/java/com/example/sesion9git/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("First commit");
+        stage.setTitle("First aaa");
         stage.setScene(scene);
         stage.show();
     }
